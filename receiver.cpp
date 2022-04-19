c1a7287f7f6d73f4a9a1a1c001f86367357efe98
Sambhav <sambhavchordia03@gmail.com>
2022-04-18 22:58:03 -0400

    schordi: started working on receiver

--- receiver.cpp
+++ receiver.cpp
@@ -7,6 +7,8 @@
 #include "connection.h"
 #include "client_util.h"
 
+bool send_and_respond(Connection &connection, Message &client_message, Message &server_response);
+
 int main(int argc, char **argv) {
   if (argc != 5) {
     std::cerr << "Usage: ./receiver [server_address] [port] [username] [room]\n";
@@ -21,13 +23,32 @@ int main(int argc, char **argv) {
   Connection conn;
 
   // TODO: connect to server
-
+  Connection connection;
+  connection.connect(server_hostname, server_port);
+  if(!connection.is_open()) {
+    std::cerr << "Failed to open connection" << "\n";
+    return 2;
+  }
   // TODO: send rlogin and join messages (expect a response from
   //       the server for each one)
+  Message server_response;
+  Message client_message(TAG_SLOGIN, username);
 
   // TODO: loop waiting for messages from server
   //       (which should be tagged with TAG_DELIVERY)
 
 
   return 0;
+}
+
+bool send_and_respond(Connection &connection, Message &client_message, Message &server_response) {
+  if(!connection.send(client_message)) {
+    std::cerr << "Client Transmission Error\n";
+    return false;
+  }
+  if(!connection.receive(server_response)) {
+    std::cerr << "Server Transmission Error\n";
+    return false;
+  }
+  return true;
 }
