5bc2c07e25336ada29787d0daac082ed1ac46fc7
Edward Wang <ewang43@jhu.edu>
2022-04-27 19:35:36 -0400

    ewang43: Modify worker function, doesnt compile because need to implement helper functions

--- server.cpp
+++ server.cpp
@@ -17,12 +17,14 @@
 // Server implementation data types
 ////////////////////////////////////////////////////////////////////////
 
-struct ConnInfo {
+struct ConnInfo
+{
   Connection *conn;
   Server *server;
 
-  ConnInfo(Connection *conn, Server *server) : conn(conn), server(server) { }
-  ~ConnInfo() {
+  ConnInfo(Connection *conn, Server *server) : conn(conn), server(server) {}
+  ~ConnInfo()
+  {
     // destroy connection when ConnInfo object is destroyed
     delete conn;
   }
@@ -32,53 +34,88 @@ struct ConnInfo {
 // Client thread functions
 ////////////////////////////////////////////////////////////////////////
 
-namespace {
+namespace
+{
 
-void *worker(void *arg) {
-  pthread_detach(pthread_self());
+  void *worker(void *arg)
+  {
+    pthread_detach(pthread_self());
 
-  ConnInfo *info_ = static_cast<ConnInfo *>(arg);
+    ConnInfo *info_ = static_cast<ConnInfo *>(arg);
 
-  // use a std::unique_ptr to automatically destroy the ConnInfo object
-  // when the worker function finishes; this will automatically ensure
-  // that the Connection object is destroyed
-  std::unique_ptr<ConnInfo> info(info_);
+    // use a std::unique_ptr to automatically destroy the ConnInfo object
+    // when the worker function finishes; this will automatically ensure
+    // that the Connection object is destroyed
+    std::unique_ptr<ConnInfo> info(info_);
+    std::string client = "";
 
-  Message msg;
+    Message msg;
 
-  if (!info->conn->receive(msg)) {
-    if (info->conn->get_last_result() == Connection::INVALID_MSG) {
-      info->conn->send(Message(TAG_ERR, "invalid message"));
-    }
-    return nullptr;
-  }
-
-  if (msg.tag != TAG_SLOGIN && msg.tag != TAG_RLOGIN) {
-    info->conn->send(Message(TAG_ERR, "first message should be slogin or rlogin"));
-    return nullptr;
-  }
-
-  std::string username = msg.data;
-  if (!info->conn->send(Message(TAG_OK, "welcome " + username))) {
-    return nullptr;
-  }
-
-  // Just loop reading messages and sending an ok response for each one
-  while (true) {
-    if (!info->conn->receive(msg)) {
-      if (info->conn->get_last_result() == Connection::INVALID_MSG) {
-        info->conn->send(Message(TAG_ERR, "invalid message"));
+    if (!info->conn->receive(msg))
+    {
+      if (info->conn->get_last_result() == Connection::INVALID_MSG)
+      {
+        info->conn->send(Message(TAG_ERR, "invalid message format"));
       }
-      break;
+      return nullptr;
     }
 
-    if (!info->conn->send(Message(TAG_OK, "this is just a dummy response"))) {
-      break;
+    if (msg.tag != TAG_SLOGIN && msg.tag != TAG_RLOGIN)
+    {
+      info->conn->send(Message(TAG_ERR, "did not receive valid login message"));
+      return nullptr;
     }
+    if (msg.tag == TAG_SLOGIN)
+    {
+      client = "slogin";
+    }
+    else
+    {
+      client = "rlogin";
+    }
+    std::string username = msg.data;
+    if (username == "")
+    {
+      info->conn->send(Message(TAG_ERR, "did not receive valid login message"));
+      return nullptr;
+    }
+
+    if (!info->conn->send(Message(TAG_OK, "logged in as " + username)))
+    {
+      return nullptr;
+    }
+
+    // Just loop reading messages and sending an ok response for each one
+    while (true)
+    {
+      if (!info->conn->receive(msg))
+      {
+        if (info->conn->get_last_result() == Connection::INVALID_MSG)
+        {
+          info->conn->send(Message(TAG_ERR, "invalid message"));
+        }
+        break;
+      }
+      if(client == "reciever")
+      {
+        chat_with_reciever(msg);
+      } 
+      else 
+      {
+        chat_with_sender(msg);
+      }
+      
+
+      // Need to implement message queue
+      // Below should go in chat_with_reciever and chat_with_sender
+      /*if (!info->conn->send(Message(TAG_OK, "this is just a dummy response")))
+      {
+        break;
+      } */
+    }
+
+    return nullptr;
   }
-
-  return nullptr;
-}
 
 }
 
@@ -87,27 +124,32 @@ void *worker(void *arg) {
 ////////////////////////////////////////////////////////////////////////
 
 Server::Server(int port)
-  : m_port(port)
-  , m_ssock(-1) {
+    : m_port(port), m_ssock(-1)
+{
   pthread_mutex_init(&m_lock, nullptr);
 }
 
-Server::~Server() {
+Server::~Server()
+{
   pthread_mutex_destroy(&m_lock);
 }
 
-bool Server::listen() {
+bool Server::listen()
+{
   std::string port = std::to_string(m_port);
   m_ssock = open_listenfd(port.c_str());
   return m_ssock >= 0;
 }
 
-void Server::handle_client_requests() {
+void Server::handle_client_requests()
+{
   assert(m_ssock >= 0);
 
-  while (true) {
+  while (true)
+  {
     int clientfd = accept(m_ssock, nullptr, nullptr);
-    if (clientfd < 0) {
+    if (clientfd < 0)
+    {
       std::cerr << "Error accepting connection\n";
       return;
     }
@@ -115,14 +157,16 @@ void Server::handle_client_requests() {
     ConnInfo *info = new ConnInfo(new Connection(clientfd), this);
 
     pthread_t thr_id;
-    if (pthread_create(&thr_id, nullptr, worker, static_cast<void *>(info)) != 0) {
+    if (pthread_create(&thr_id, nullptr, worker, static_cast<void *>(info)) != 0)
+    {
       std::cerr << "Could not create thread\n";
       return;
     }
   }
 }
 
-Room *Server::find_or_create_room(const std::string &room_name) {
+Room *Server::find_or_create_room(const std::string &room_name)
+{
   // this function can be called from multiple threads, so
   // make sure the mutex is held while accessing the shared
   // data (the map of room names to room objects)
@@ -131,11 +175,14 @@ Room *Server::find_or_create_room(const std::string &room_name) {
   Room *room;
 
   auto i = m_rooms.find(room_name);
-  if (i == m_rooms.end()) {
+  if (i == m_rooms.end())
+  {
     // room does not exist yet, so create it and add it to the map
     room = new Room(room_name);
     m_rooms[room_name] = room;
-  } else {
+  }
+  else
+  {
     room = i->second;
   }
 
